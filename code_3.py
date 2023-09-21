88b0dd0de51ebd4c179bc8c9b4b6d852493fe74a
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 00:46:39 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+import random
+def generate_random_data():
+
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    for item in data:
+    return data
+    main()
+
+    data.forEach(item => {
+function main() {
+    const data = generateRandomData();
+main();
+}
+    return data;
+
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    }
+
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    const data = [];
